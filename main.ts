575c48b1c4a4d92f4aa1f908500bfc7529e0451f
aida0710 <51500566+aida0710@users.noreply.github.com>
2023-09-26 19:01:26 +0900

    整形

--- main.ts
+++ main.ts
@@ -1,15 +1,15 @@
 console.time()
 for (let i = 1; i < 250; i++) {
-    if ((i % 3 === 0) && (i % 5 === 0)){
-        console.log("fizzbuzz | " + i);
+    if ((i % 3 === 0) && (i % 5 === 0)) {
+        console.log(i + " | fizzbuzz");
         continue;
     }
-    if ((i % 3 === 0)){
-        console.log("fizz | " + i);
+    if ((i % 3 === 0)) {
+        console.log(i + " | fizz");
         continue
     }
-    if ((i % 5 === 0)){
-        console.log("buzz | " + i);
+    if ((i % 5 === 0)) {
+        console.log(i + " | buzz");
         continue
     }
     console.log(i)
